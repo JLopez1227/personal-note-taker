85182ef70fa2e1a04f6545055fdcce3594761f11
JLopez1227 <josuenmilopez27@gmail.com>
2023-07-25 22:10:29 -0500

    Worked on post and get routes to get new notes posted and added to the page.

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs')
+const util = require('util')
 const dbData = require('./db/db.json');
+const dbPath = './db/db.json'
 const PORT = process.env.PORT || 3001 ;
-
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
 const app = express();
 
 // Sets up the Express app to handle data parsing
@@ -15,7 +19,26 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
-app.get('/api/notes', (req, res) => res.json(dbData));
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/notes.html'));
+  });
+
+app.get('/api/notes', (req, res) => {
+  readFileAsync(dbPath, 'utf8').then(notes => {
+    return res.json(JSON.parse(notes));
+  })
+});
+
+app.post('/api/notes', (req, res) => {
+  readFileAsync(dbPath, 'utf8').then(notes => {
+    writeFileAsync(dbPath, JSON.stringify([...JSON.parse(notes), req.body])).then(data => {
+      return res.json({
+        message: 'Note successfully added!'
+      })
+    })
+  })
+  
+})
 
 app.listen(PORT, () => {
   console.log(`Note Taker App listening at http://localhost:${PORT}`);
